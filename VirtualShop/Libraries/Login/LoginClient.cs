d733ec600d5c88e7b2b4a1cab04f949344ac69a7
VictorLCosta <victorlc2019@outlook.com>
2020-09-09 16:36:04 -0300

    LoginClient class configured

---
+++ VirtualShop/Libraries/Login/LoginClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualShop.Libraries.Session;
+using VirtualShop.Models;
+using Newtonsoft.Json;
+
+namespace VirtualShop.Libraries.Login
+{
+    public class LoginClient
+    {
+        private Session.Session _session;
+        private string Key = "Login.Client";
+
+        public LoginClient(Session.Session session)
+        {
+            _session = session;
+        }
+
+        public void Login(Client client) 
+        {
+            string clientJson = JsonConvert.SerializeObject(client);
+
+            _session.Create(Key, clientJson);
+        }
+
+        public Client GetClient() 
+        {
+            string clientJson = _session.Read(Key);
+            
+            return JsonConvert.DeserializeObject<Client>(clientJson);
+        }
+
+        public void Logout()
+        {
+            _session.RemoveAll();
+        }
+    }
+}
