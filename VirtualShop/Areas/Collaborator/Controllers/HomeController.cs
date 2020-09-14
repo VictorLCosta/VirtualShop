e881ff729d105ec3762cf52e142263ad5490609e
VictorLCosta <victorlc2019@outlook.com>
2020-09-14 14:08:15 -0300

    Collaborator Area - Login Action

--- VirtualShop/Areas/Collaborator/Controllers/HomeController.cs
+++ VirtualShop/Areas/Collaborator/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace VirtualShop.Areas.Collaborator.Controllers
 {
+    [Area("Collaborator")]
     public class HomeController : Controller
     {
         public IActionResult Login()
