63792eaa2d090b91be7d86e193b7cd8360fe427f
VictorLCosta <victorlc2019@outlook.com>
2020-10-13 14:33:01 -0300

    Collaborator Area - Collaborator Model and Controller

---
+++ VirtualShop/Areas/Collaborator/Controllers/CollaboratorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualShop.Areas.Collaborator.Controllers
+{
+    public class CollaboratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromForm]Models.Collaborator collaborator)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromForm] Models.Collaborator collaborator, int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
--- VirtualShop/Models/Collaborator.cs
+++ VirtualShop/Models/Collaborator.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VirtualShop.Models
 {
     public class Collaborator
     {
+        [Display(Name = "Código")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome")]
+        [MinLength(4, ErrorMessage = "Insira no mínimo 4 caracteres")]
         public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Informe o e-mail")]
+        [EmailAddress(ErrorMessage = "Insira um e-mail válido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [MinLength(6, ErrorMessage = "Insira no mínimo 6 caracteres")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [NotMapped]
+        public string PasswordConfirm { get; set; }
+
+        /*
+         Tipo
+        - C = Comum
+        - G = Gerente
+         */
         public char Type { get; set; }
 
         public Collaborator()
