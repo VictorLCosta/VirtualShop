65f69a2b16f8785ebb0d65492d2c7fe05f959490
VictorLCosta <victorlc2019@outlook.com>
2021-02-10 15:28:32 -0300

    Collaborator Area - Money JQuery mask

--- VirtualShop/Models/Product.cs
+++ VirtualShop/Models/Product.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace VirtualShop.Models
@@ -7,7 +8,11 @@ namespace VirtualShop.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Display(Name="Descrição")]
         public string Desc { get; set; }
+
+        [Display(Name="Preço")]
         public decimal Value { get; set; }
         public int Qty { get; set; }
 
@@ -18,6 +23,7 @@ namespace VirtualShop.Models
         public double Length { get; set; }
         #endregion
         
+        [Display(Name="Código da categoria")]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
--- VirtualShop/wwwroot/resources/js/Actions.js
+++ VirtualShop/wwwroot/resources/js/Actions.js
@@ -6,4 +6,5 @@
             e.preventDefault();
         }
     });
+    $('.money').mask('000.000.000.000.000,00', {reverse: true});
 });
